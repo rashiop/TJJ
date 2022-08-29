061ab552dd105fd2b47780e4d8c2a5489d58dee5
Poppy Sari <poppy.sari@rga.com>
2022-08-29 10:28:51 +0700

    feat: fit in the viewport

--- readme.md
+++ readme.md
@@ -1,16 +1,31 @@
-# Three.js Journey
+# Fullscreen and Resizing
 
-## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
-
-``` bash
-# Install dependencies (only the first time)
-npm install
-
-# Run the local server at localhost:8080
-npm run dev
-
-# Build for production in the dist/ directory
-npm run build
+## Fit in the viewport
+As simple as window.<innerSize> below? But there is margin top left due to browser default setting :)
+```js
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 ```
+add some css, cause we might need to use `controls.enabled = false` and mac trackpad managed to scroll :(
+```css
+html, body {
+  overflow: hidden;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.webgl {
+  position: fixed;
+  top: 0;
+  left: 0;
+  outline: none;
+}
+```
+## Handle Resize
+## Handle Pixel Ratio
+## Handle Fullscreen
--- src/script.js
+++ src/script.js
@@ -23,8 +23,8 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
 /**
@@ -43,7 +43,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 
@@ -52,18 +52,17 @@ renderer.setSize(sizes.width, sizes.height)
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
