cf787a7aa809e9fff6a3d74583ff6a6256bc838b
Poppy Sari <poppy.sari@rga.com>
2022-08-30 15:28:16 +0700

    feat: complete the basic feature without peeking on the video

--- readme.md
+++ readme.md
@@ -4,3 +4,38 @@ What
 - algorithm written on shaders
 - good news? just use built-in material :D
 
+## Material 101
+```js
+// reuseable
+const material = new THREE.MeshBasicMaterial({
+  map: texture,
+  color: <<color>>,
+  transparent: true,
+  opacity: 0.5,
+}) 
+```
+
+- MeshBasicMaterial
+  - applies a uniform color / texture on geo
+  - not affected by light
+- MeshDepthM
+  - show depth based on camera near & far plane, monochrome
+  - white near
+  - black far
+- [MeshDistanceM](https://threejs.org/docs/index.html?q=material#api/en/materials/MeshDistanceMaterial)
+  - implmeneting shadow mapping w/ PointLights
+- [MeshLambertM](https://threejs.org/docs/index.html?q=material#api/en/materials/MeshLambertMaterial)
+  - FOR: non-shiny surfaces, wo specular highlights
+- MeshMatcapM
+- MeshNormalM
+  - map normal vectors to RGB colors
+  
+- MeshPhongM
+- MeshPhysicalM
+- MeshStandardM
+- MeshToonM
+- PointsM
+- RawShaderM
+- ShaderM
+- ShadowM
+- SpriteM
--- src/script.js
+++ src/script.js
@@ -12,26 +12,48 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Texture
+ */
+const doorNormalTexture = new THREE.TextureLoader().load('/textures/door/normal.jpg')
+const material = new THREE.MeshBasicMaterial({
+  map: doorNormalTexture,
+  color: 0xff0000,
+})
+
+/**
+ * Objects
+ */
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material)
+sphere.position.set(-1.5, 0, 0)
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material)
+
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.1, 10, 50), material)
+torus.position.set(1.5, 0, 0)
+
+scene.add(sphere, plane, torus)
+
+/**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -52,7 +74,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -62,18 +84,25 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  // Update rotation
+  sphere.rotation.y = elapsedTime * 0.1
+  plane.rotation.y = elapsedTime * 0.1
+  torus.rotation.y = elapsedTime * 0.1
 
-    // Update controls
-    controls.update()
+  sphere.rotation.x = elapsedTime * 0.1
+  plane.rotation.x = elapsedTime * 0.1
+  torus.rotation.x = elapsedTime * 0.1
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
