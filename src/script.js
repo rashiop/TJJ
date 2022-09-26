0acb8bf504fbd0afad690dea33bab7580fe44c0d
Poppy Sari <poppy.sari@rga.com>
2022-09-26 17:36:23 +0700

    feat(texture): add wall bricks

--- src/script.js
+++ src/script.js
@@ -19,6 +19,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
@@ -27,11 +28,27 @@ const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
+const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
+const bricksAmbientOcclusionTexture = textureLoader.load('/bricks/door/ambientOcclusion.jpg')
+const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
+const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
+
 /**
  * House
  */
 
-const wall = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ color: 0x7f5748 }))
+const wall = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshStandardMaterial({
+    color: 0x7f5748,
+    normalMap: bricksNormalTexture,
+    aoMap: bricksAmbientOcclusionTexture,
+    roughnessMap: bricksRoughnessTexture,
+    map: bricksColorTexture,
+  })
+)
+wall.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(wall.geometry.attributes.uv.array, 2))
+
 wall.position.y = 1.5
 
 /// Roof
