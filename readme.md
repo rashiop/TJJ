187a235a019e6417ed2df86b7a5727eb10017158
Poppy Sari <poppy.sari@rga.com>
2022-10-06 09:59:24 +0700

    feat(galaxy): add basic shape

--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
-## What 
-Threejs Journey - Code along
+# Galaxy Generator 
+
+## Steps
+1. lil-gui
+- `onFinishChange` - callback to after value change on gui
+```js
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+```
+2. destroy old one
+- after re-add to screen
+- need to remove manually
+```js
+    if(points !== null) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+```
--- src/script.js
+++ src/script.js
@@ -16,35 +16,78 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Components
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+
+/** Particles */
+const parameters = {}
+parameters.count = 1000
+parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
+
+let points = null
+let materials = null
+let geometry = null
+const generateGalaxy = () => {
+  // destroy old galaxy
+  if (points !== null) {
+    geometry.dispose()
+    materials.dispose()
+    scene.remove(points)
+  }
+
+  geometry = new THREE.BufferGeometry()
+  materials = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+  })
+
+  const positions = new Float32Array(parameters.count * 3)
+  for (let i = 0; i < parameters.count; i++) {
+    const radius = Math.random() * parameters.radius
+    const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2
+
+    const i3 = i * 3
+    positions[i3] = radius
+    positions[i3 + 1] = Math.cos(branchAngle) * radius
+    positions[i3 + 2] = Math.sin(branchAngle) * radius
+  }
+
+  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+  points = new THREE.Points(geometry, materials)
+  scene.add(points)
+}
+generateGalaxy()
+
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -65,7 +108,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -75,18 +118,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
