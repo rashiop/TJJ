1a8b09081cb7b280f3c57bbdd894e121641fb24d
Poppy Sari <poppy.sari@rga.com>
2022-11-11 13:49:12 +0700

    feat: add model

--- readme.md
+++ readme.md
@@ -1,1 +1,112 @@
 # Imported Model
+- Blender is easier on complex model :))
+- [Formats](https://en.wikipedia.org/wiki/List_of_file_formats#3D_graphics), popular:
+  - OBJ
+  - FBX
+  - STL
+  - PLY
+  - COLLADA
+  - 3DS
+  - GLTF
+
+## GLTF
+- GL Transmission Format
+- by Khronos Group (team behind OpenGL, WebGL, Vulkan, Collada and with many members like AMD / ATI, Nvidia, Apple, id Software, Google, Nintendo, etc)
+- It supports very different sets of data. You can obviously have data like the geometries and the materials but you can also have data like cameras, lights, scene graph, animations, skeletons, morphing and even multiple scene.
+- STD 4 real-time => supported on most sw, engine, lib
+- Use it everywhere?
+    - NO!
+    - heavy
+    - simple geometry just use OBJ, FBX, STL, or PLY
+- Free sample [model](https://github.com/KhronosGroup/glTF-Sample-Models)
+- formats:
+  - [default] glTF
+    - we only load .gltf -> reference to __
+      - `glTF` = JSON w/ camera, lights, scenes, materials, object transformations, but not geo/textures
+      - `.bin` has UV coord, normals, vertex colors
+      - `.png` as texture
+  - glTF-Binary
+    - 1 file
+    - binary file
+    - PROS: lighter
+    - CONS: cant altered ezly
+  - glTF-Draco
+    - like default
+    - but buffer data 
+      - compressed using Draco algo
+        - dracocompression is not exclusive to gltf
+        - algo created by google
+      - Need `DRACOLoader` to load
+    - lighter
+  - glTF-Embedded
+    - 1 file
+    - but JSON
+    - PROS: ezly editable
+  - sometimes hidden by the OS !!?
+- I choose youuuu GLTF format
+  - how asset handled
+  - Alter textures / coords of the lights after exporting -> glTF-default
+    - presents the adv of loading the diff files separately
+    - load speed improv
+  - 1 file only & wont modify -> glTF-Binary.
+  - Both case, decide whether Draco or not
+    - draco lightweight
+    - more time to decode
+    - if small model, might not need the Draco
+
+
+### Use draco loader
+The decoder is available in native JavaScript but also Web Assembly (wasm), and it can run in a worker (another thread as we saw at the end of the Physics lesson). Those two features significantly improve performances, but they imply having a wholly separated code.
+
+Three.js already provided this seperated code. To find it, we must browse into the Three.js dependency and copy the Draco decoder folder into our /static/ folder.
+
+This Draco folder is located in `/node_modules/three/examples/js/libs/`. Take the whole `/draco/` folder and copy it into your `/static/` folder. We can now provide the path to this folder to our dracoLoader:
+
+
+```js
+// cheap import
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const dracoLoader = new DRACOLoader()
+// copy /node_modules/three/examples/js/libs/draco to /static/draco
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+let mixer = null
+gltfLoader.load('/models/Fox/glTF-Draco/Fox.gltf', (gltf) => {
+  // scaling the model
+  gltf.scene.scale.set(0.03, 0.03, 0.03)
+  scene.add(gltf?.scene)
+
+  // animate model
+  mixer = new THREE.AnimationMixer(gltf.scene)
+  const action = mixer.clipAction(gltf.animations[0])
+  action.play()
+},
+(progress) => {},
+(error) => {}
+)
+
+// if using draco loader
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+// update animation on tick
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
+
+  // Update animations
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
+
+  // ...
+}
+
+```
--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
 import * as dat from 'lil-gui'
 
 /**
@@ -16,18 +19,38 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Models
+ */
+
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+let mixer = null
+gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
+  gltf.scene.scale.set(0.005, 0.005, 0.005)
+  scene.add(gltf?.scene)
+
+  // animate model
+  mixer = new THREE.AnimationMixer(gltf.scene)
+  const action = mixer.clipAction(gltf.animations[2])
+  action.play()
+})
+
+gltfLoader.setDRACOLoader(dracoLoader)
+/**
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0,
+    roughness: 0.5,
+  })
 )
 floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x = -Math.PI * 0.5
 scene.add(floor)
 
 /**
@@ -40,10 +63,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
@@ -51,23 +74,22 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -87,7 +109,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -100,20 +122,24 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  // Update animations
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
